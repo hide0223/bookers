51b3cb6261382cf8a8bd9078dbf4865f7abca2d5
EC2 Default User <ec2-user@ip-172-31-39-110.ec2.internal>
2024-06-09 10:51:54 +0000

    bookers finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,48 @@
 class BooksController < ApplicationController
   def new
+    @books = Book.all
     @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :new
+    end
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id]) 
   end
 
   def edit
+     @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  def destroy
+    book = Book.find(params[:id]) 
+    book.destroy  
+    redirect_to '/books'   
+  end
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get '/books' => 'books#new'
-  post 'books' => 'books#create'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  # get '/books' => 'books#new', as: 'new_book'
+  # post 'books' => 'books#create'
+  # get 'books/index'
+  # get 'books/:id' => 'books#show', as: 'book'
+  # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  # patch 'books/:id' => 'books#update', as: 'update_book'
+  # get root 'homes#top'
+  # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  
   get root 'homes#top'
+  resources :books
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
